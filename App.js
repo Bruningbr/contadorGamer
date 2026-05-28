4431fafdda083157956082564707a9b365a8d9b4
Bruningbr <brunoandradeoliveira40@gmail.com>
2026-05-28 10:11:45 -0300

    Aula contadorGamer 28/05

--- App.js
+++ App.js
@@ -1,34 +1,60 @@
-// Importação para o menu lateral funcionar
+//Importação para o menu lateral funcionar
 import 'react-native-gesture-handler';
 
-// Imporar o container da navegação
-// ele controla todas as telas do app
+//Importar o container de navegação
+// Ele controla todas as telas do app
 import { NavigationContainer } from '@react-navigation/native';
 
-// Importa o menu lateral (DREWER)
+//Importar o menu lateral (DRAWER)
 import { createDrawerNavigator } from '@react-navigation/drawer';
 
-// Importa as telas
+//Importar as telas
 import Jogador1 from './components/Jogador1';
+import Jogador2 from './components/Jogador2';
+import Jogador3 from './components/Jogador3';
+import Atividade from './components/Atividade';
 
-// Criar o Drawer
+//Cria o Drawer
 const Drawer = createDrawerNavigator();
 
-// Componente principal APP
-export default function App() {
-  // o que esta dentro do return aparece na tela
+//Componente principal do APP
+export default function APP() {
+  // O que está dentro do return aparece na tela
   return (
     // Container Principal da navegação
     <NavigationContainer>
       {/* menu lateral */}
       <Drawer.Navigator>
-        {/* Tela do jogador 1 */}
+        {/* Tela do Jogador 1 */}
         <Drawer.Screen
-        // nome que aparece no menu
-         name="Jogador 1" 
-        // componente que será aberto
-         component={Jogador1} 
-         />
+          // nome que aparece no menu
+          name="1 Jogador"
+          //componente que será aberto
+          component={Jogador1}
+
+        />
+        <Drawer.Screen
+          // nome que aparece no menu
+          name="2 Jogadores"
+          //componente que será aberto
+          component={Jogador2}
+
+        />
+
+        <Drawer.Screen
+          // nome que aparece no menu
+          name="3 Jogadores"
+          //componente que será aberto
+          component={Jogador3}
+
+        />
+           <Drawer.Screen
+          // nome que aparece no menu
+          name="Atividade"
+          //componente que será aberto
+          component={Atividade}
+
+        />
       </Drawer.Navigator>
     </NavigationContainer>
   );
